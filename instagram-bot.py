8d5a9ffa976c81d09a10f8798bc0774945bc9b2f
1337Anton <54065258+1337Anton@users.noreply.github.com>
2019-08-30 14:33:34 +0200

    Add files via upload

--- instagram-bot.py
+++ instagram-bot.py
@@ -1,16 +1,61 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 from bs4 import BeautifulSoup as bs
 import time
-import re
 from urllib.request import urlopen
+import re
 
-email = input("\nWhat is your Instagram email adress? ")
-email.strip()
-password = input("\nWhat is your Instagram password? ")
-password.strip()
-hashtag = input("\nWhich subject should the posts you like have? ")
-hashtag.replace(" ", "")
+def split_str(s):
+  return [ch for ch in s]
+
+emailGood = False
+passwordGood = False
+hashtagGood = False
+
+goodCharsEmail = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "-", "_", "@"]
+
+print("\n")
+print(" .___                __                                              __________        __    ")
+print(" |   | ____   ______/  |______    ________________    _____          \______   \ _____/  |_  ")
+print(" |   |/    \ /  ___|   __\__  \  / ___\_  __ \__  \  /     \   ______ |    |  _//  _ \   __\ ")
+print(" |   |   |  \\___ \ |  |  / __ \/ /_/  >  | \// __ \|  Y Y  \ /_____/ |    |   (  <_> )  |   ")
+print(" |___|___|  /____  >|__| (____  |___  /|__|  (____  /__|_|  /         |______  /\____/|__|   ")
+print("         \/     \/           \/_____/            \/      \/                 \/               ")
+print("\n \n")
+print("With the Instagram Bot you can like alot of posts including a specific hashtag.")
+print("\n")
+
+while emailGood == False:
+    email = input("What is your Instagram email adress? \n")
+    email.strip()
+    newEmail = split_str(email)
+    check = bool(set(newEmail).intersection(goodCharsEmail))
+    if check == True:
+        emailGood = True
+    elif len(email) >= 3 and len(email) <= 256:
+        emailGood = True
+    else:
+        emailGood = False
+        print("This isn't a valid E-Mail adress.")
+
+
+while passwordGood == False:
+    password = input("What is your Instagram password? \n")
+    password.strip()
+    if len(password) <=8:
+        passwordGood = False
+        print("This isn't a valid Instagram password.")
+    else:
+        passwordGood = True
+
+while hashtagGood == False:
+    hashtag = input("Posts with which hashtag do you want to like? \n")
+    hashtag.replace(" ", "")
+    hashtag.replace("#", "")
+    hashtagGood = True
 
 class InstagramBot:
 
@@ -22,7 +67,7 @@ class InstagramBot:
     def login(self):
         bot = self.bot
         bot.get("https://www.instagram.com/accounts/login/")
-        time.sleep(2)
+        time.sleep(1)
         email = bot.find_element_by_name("username")
         password = bot.find_element_by_name("password")
         email.clear()
@@ -32,15 +77,14 @@ class InstagramBot:
         password.send_keys(self.password)
         time.sleep(0.25)
         password.send_keys(Keys.RETURN)
-        time.sleep(2)
 
     def search(self, hashtag):
         bot = self.bot
-        pop = 1
-        while pop >= 1:
+        try:
+            notifications = WebDriverWait(bot, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "HoLwm ")))
+        finally:
             notifications = bot.find_element_by_class_name("HoLwm ")
             notifications.click()
-            pop = 0
         time.sleep(4)
         bot.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
         time.sleep(2)
@@ -57,14 +101,24 @@ class InstagramBot:
             for link in script.findAll('a'):
                 if re.match("/p", link.get('href')):
                     links.append('https://www.instagram.com'+link.get('href'))
-            time.sleep(3)
+
+        print("\n")
         print(links)
+        print("\n")
+        len(links)
+
         for link in links:
             bot.get(link)
             time.sleep(2)
-            likeButton = bot.find_element_by_class_name("u-__7")
+            likeButton = bot.find_element_by_class_name("glyphsSpriteHeart__outline__24__grey_9")
             likeButton.click()
-            time.sleep(4)
+            time.sleep(2)
+           # commentButton = bot.find_element_by_class_name("Ypffh")
+           # commentButton.click()
+           # time.sleep(1)
+           # commentButton.send_keys(comment)
+           # commentButton.send_keys(Keys.RETURN)
+           # time.sleep(2)
 
 start = InstagramBot(email, password)
 start.login()
