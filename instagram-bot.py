4501ba115b5087023e2e3bc4c071a9bfaf626daf
1337Anton <54065258+1337Anton@users.noreply.github.com>
2019-08-30 22:46:06 +0200

    Add files via upload

--- instagram-bot.py
+++ instagram-bot.py
@@ -9,14 +9,9 @@ from urllib.request import urlopen
 import re
 from validate_email import validate_email
 
-def split_str(s):
-  return [ch for ch in s]
-
 email_good = False
 password_good = False
 hashtag_good = False
-
-good_chars_email = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "-", "_", "@"]
 
 print("\n")
 print(" .___                __                                              __________        __    ")
